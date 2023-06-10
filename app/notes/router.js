e0de4cee0d5b8a2527cff761e5581d7b99c8112c
Abdulla Al Shahid <shahid.dev.cse@gmail.com>
2023-06-10 23:46:30 +0600

    Update router.js

--- app/notes/router.js
+++ app/notes/router.js
@@ -1,5 +1,6 @@
 const notes_router = require("express").Router();
-const { create_notes, get_all_notes } = require("./controller");
+const { create_notes, get_all_notes,get_note_by_email } = require("./controller");
 notes_router.post("/create/note", create_notes);
 notes_router.get("/get/all/note", get_all_notes);
+notes_router.get("/get/notes/:email", get_note_by_email);
 module.exports = notes_router;
